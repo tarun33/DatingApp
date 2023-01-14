9b1c1c5dd50dec57cff8eb7816207c51fd09c878
Tarun <tarunkrai@gmail.com>
2023-01-14 22:08:29 +0530

    End of Section 7

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -22,7 +22,7 @@ export class NavComponent implements OnInit {
     this.accountService.login(this.model).subscribe({
       next: () => this.router.navigateByUrl('/members'),
 
-      error: (error) => this.toastr.error(error.error)
+      
     });
   }
 
