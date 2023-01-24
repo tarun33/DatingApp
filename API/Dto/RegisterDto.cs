76fb9b09ca1a7b85535b11aa80e167d3c30eb50f
Tarun <tarunkrai@gmail.com>
2023-01-24 16:54:53 +0530

    End of section 12

--- API/Dto/RegisterDto.cs
+++ API/Dto/RegisterDto.cs
@@ -3,13 +3,24 @@ using System.ComponentModel.DataAnnotations;
 namespace API.Dto
 {
   public class RegisterDto
-    {
-        [Required]
-        public string Username { get; set; }
+  {
+    [Required]
+    public string Username { get; set; }
 
-        [Required]
-        [StringLength(8,MinimumLength =4)]
-        public string Password { get; set; }
 
-    }
+    [Required]
+    public string KnownAs { get; set; }
+    [Required]
+    public string Gender { get; set; }
+    [Required]
+    public DateOnly? DateOfBirth { get; set; } //optional to make required work!
+    [Required]
+    public string city { get; set; }
+    [Required]
+    public string Country { get; set; }
+
+    [StringLength(8, MinimumLength = 4)]
+    public string Password { get; set; }
+
+  }
 }
